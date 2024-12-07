1f5135dbbaa2a6527bfb98bfcf88d1f76ad12a38
revin de castro <redc1026@gmail.com>
2024-12-07 18:31:27 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import react from "react";
+import ExpandableCardDemo from "./components/show.jsx"
+
+function App() {
+  return (
+    <div className="App h-screen flex justify-center items-center">
+      <ExpandableCardDemo />
+    </div>
+  );
+}
+
+export default App;
